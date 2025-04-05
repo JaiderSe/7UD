acd18363f9580af78983046da299c466bce6f9ee
Sebastián Quintero <154089423+JaiderSe@users.noreply.github.com>
2025-04-05 16:39:42 -0500

    ejercicio2

---
+++ kotlin/Ejercicio2.kt
@@ -0,0 +1,51 @@
+package com.example.variables
+
+import kotlin.math.PI
+import kotlin.math.sin
+import kotlin.math.cos
+
+
+fun main (){
+
+
+    var nombre1:String = "Juan"
+    var nombre2:String = "Camilo"
+    var apellido1:String = "Jimenez"
+    var apellido2:String = "Moreno"
+    var edad:Int = 20
+    var sexo:Char = 'M'
+    var cedula:String = "1000000000"
+    var celular:String = "3100000000"
+    var operador:String = "Movistar"
+    var direction:String = "Calle 104-sur n 5-20"
+    var mail:String = "william.henry.harrison@example-pet-store.com"
+
+    var Datos: String = "\n Su edad es: "+edad+"\n Su sexo es:"+sexo+"\n Su cedula es:"+cedula+"\nsu dirección es:"+direction
+    var nombreCompleto:String = nombre1+" "+nombre2+" "+apellido1+" "+apellido2
+    var plan_celular: String = "\nSu celular es: "+celular+" \nSu operador es: "+operador+" \nSu correo electrónico es: "+mail
+
+
+
+    print("Nombre completo: "+nombreCompleto)
+    print(Datos)
+    print(plan_celular)
+
+
+
+
+
+
+
+
+}
+
+// video:
+/**
+ * video:
+ *
+ * https://udistritaleduco-my.sharepoint.com/:v:/g/personal/jsmorenoq_udistrital_edu_co/ETpPZUU5ht1AgtHioflXa7UBUoEbe3jcEHSFx7Wss5nRkw?e=jMfF1r&nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJTdHJlYW1XZWJBcHAiLCJyZWZlcnJhbFZpZXciOiJTaGFyZURpYWxvZy1MaW5rIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXcifX0%3D
+ *
+ * */
+
+
+
